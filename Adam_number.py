ebd86f1ff0a0f12db07d847e32c3056177aa6fdd
Vijayalakshmiboya <98221516+Vijayalakshmiboya@users.noreply.github.com>
2022-05-26 15:52:06 +0530

    n=int(input()) sum=0 r1=0 r2=0 sq1=n*n while(n>0):     r1=r1*10     r1=r1+n%10     n=n//10 sq2=r1*r1 while(sq2>0):     r2=r2*10     r2=r2+sq2%10     sq2=sq2//10 if(sq1==r2):     print("True") else:     print("False")

---
+++ Adam_number.py
@@ -0,0 +1,18 @@
+n=int(input())
+sum=0
+r1=0
+r2=0
+sq1=n*n
+while(n>0):
+    r1=r1*10
+    r1=r1+n%10
+    n=n//10
+sq2=r1*r1
+while(sq2>0):
+    r2=r2*10
+    r2=r2+sq2%10
+    sq2=sq2//10
+if(sq1==r2):
+    print("True")
+else:
+    print("False")
